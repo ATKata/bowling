323fe3e3a5fe8af113df7c53ec09a3d594425b71
U-AT\Alec.Tunbridge <Alec.Tunbridge@MCR-l31070.at.autotrader.co.uk>
2014-10-29 21:52:11 +0000

    Results of first session

--- src/main/java/kata/bowling/Game.java
+++ src/main/java/kata/bowling/Game.java
@@ -1,11 +1,32 @@
 package kata.bowling;
 
-public class Game {
-    public void roll(int pins) {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Created by john.oldfield on 24/10/2014.
+ */
+
+
+public class Game {
+
+    private int score;
+    private List<Integer> rolls = new ArrayList<Integer>();
+
+    public void roll(int pins){
+        rolls.add(pins);
+        score += pins;
     }
 
     public int score(){
-        return 0;
+        return score;
+    }
+
+    public int scoreFrame(int frameNo){
+        if(rolls.get(0) + rolls.get(1) == 10){
+            return rolls.get(0) + rolls.get(1) + rolls.get(2);
+        } else {
+            return rolls.get(0) + rolls.get(1);
+        }
     }
 }
--- src/test/java/kata/bowling/GameTest.java
+++ src/test/java/kata/bowling/GameTest.java
@@ -1,16 +1,60 @@
 package kata.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * Created by john.oldfield on 24/10/2014.
+ */
 public class GameTest {
 
-    @Test
-    public void testRoll() throws Exception {
+    private Game bowlingGame;
+
+    @Before
+    public void setUp(){
+        bowlingGame = new Game();
     }
 
     @Test
-    public void testScore() throws Exception {
-
+    public void testBowlZero(){
+        bowlingGame.roll(0);
+        assertEquals(0, bowlingGame.score());
     }
+
+    @Test
+    public void testBowlOneExpectsScoreOne(){
+        bowlingGame.roll(1);
+        assertEquals(1,bowlingGame.score());
+    }
+
+    public void testGutterGame(){
+        for(int i=0; i<20; i++){
+            bowlingGame.roll(0);
+        }
+        assertEquals(0,bowlingGame.score());
+    }
+
+    public void testScoreFirstFrameNoBonuses(){
+        bowlingGame.roll(1);
+        bowlingGame.roll(2);
+        bowlingGame.roll(3);
+        assertEquals(3,bowlingGame.scoreFrame(1));
+    }
+
+    public void testScoreFirstFrameNoBonuses2(){
+        bowlingGame.roll(9);
+        bowlingGame.roll(0);
+        bowlingGame.roll(3);
+        assertEquals(9,bowlingGame.scoreFrame(1));
+    }
+
+    public void testScoreFirstFrameSpare(){
+        bowlingGame.roll(9);
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+        assertEquals(11,bowlingGame.scoreFrame(1));
+    }
+
+// test combining 2 frames
 }
